a8e4c881d4af132de3a3954b4496fcfd73177b0c
Eric Pantera <eric.pantera@gmail.com>
2013-12-27 16:11:59 +0100

    Add assets

--- lib/rorschart/engine.rb
+++ lib/rorschart/engine.rb
@@ -4,6 +4,7 @@ module Rorschart
     initializer "precompile", :group => :all do |app|
       # use a proc instead of a string
       app.config.assets.precompile << Proc.new{|path| path == "rorschart.js" }
+      app.config.assets.precompile << Proc.new{|path| path == "rorschart.css" }
     end
 
     initializer "helper" do |app|
