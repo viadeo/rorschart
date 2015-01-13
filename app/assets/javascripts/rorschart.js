d9e49493333949d0acefc681714b73c9d3f52a9d
Victor Ceron <vceron@viadeoteam.com>
2015-01-13 09:49:09 +0100

    Hide/show columnds - init

--- app/assets/javascripts/rorschart.js
+++ app/assets/javascripts/rorschart.js
@@ -58,18 +58,30 @@
 
   function drawChart(chart, chartClass, element, dataSource, options) {
 
-    var dataTable, type;
+    var dataTable, type, wrapper, columns = [0],  columnsMap = {};
 
     type = Object.prototype.toString.call(dataSource);
     if (type === '[object String]') {
       dataSource = JSON.parse(dataSource);
     }
 
+    console.log('dataSource toString:',dataSource);
+
     dataSource = Object.create(dataSource);
     if (dataSource.cols == null) {
       setText(element, "Empty data")
       return
     }
+
+    console.log('datasource.length',dataSource.cols.length);
+    for (var i = 0; i < dataSource.cols.length; i++) {
+      console.log('dataSource.cols['+i+']',dataSource.cols[i]);
+      // var col = seriesMap[i].column;
+      // columnsMap[col] = i;
+      columns.push(dataSource.cols[i]);
+    }
+
+    console.log('Object dataSource:',dataSource);
 
     try {
       dataTable = new google.visualization.DataTable(processDate(dataSource));
@@ -80,8 +92,74 @@
 
     chart.chart = new chartClass(element);
 
+
+    function showHideSeries () {
+        var sel = chart.chart.getSelection();
+        console.log('selectedItem:',sel);
+        // if selection length is 0, we deselected an element
+        console.log('selected ? ',sel.length);
+        if (sel.length > 0) {
+
+            // if row is undefined, we clicked on the legend
+            console.log('sel[0].row',sel[0].row);
+            if (sel[0].row == null) {
+                console.log('sel[0].column:',sel[0].column);
+                var col = sel[0].column;
+                if (typeof(columns[col]) == 'number') {
+                    var src = columns[col];
+
+                    // hide the data series
+                    columns[col] = {
+                        label: data.getColumnLabel(src),
+                        type: data.getColumnType(src),
+                        sourceColumn: src,
+                        calc: function () {
+                            return null;
+                        }
+                    };
+
+                    // grey out the legend entry
+                    //dataSource[columnsMap[src]].color = '#CCCCCC';
+                }
+                else {
+                    //var src = columns[col].sourceColumn;
+
+                    // show the data series
+                    columns[col] = src;
+                    //dataSource[columnsMap[src]].color = null;
+                }
+                var view = new google.visualization.DataView(dataTable) || {};
+                view.columns = columns;
+                chart.chart.setView(view);
+                chart.chart.draw();
+            }
+        }
+    }
+
+   wrapper = new google.visualization.ChartWrapper({
+        chartType: 'ColumnChart',
+        dataTable: dataTable,
+        options: options,
+        containerId: element
+      });
+
+
+    google.visualization.events.addListener(wrapper, 'select', showHideSeries);
+
+    var view = {
+        columns: columns
+    };
+
+    console.log('chartClass',chartClass);
     resize(function () {
-      chart.chart.draw(dataTable, options);
+      //chart.chart.draw(dataTable, options);
+
+      // let's use the wrapper now
+      wrapper.draw();
+      var view_ = new google.visualization.DataView(dataTable)
+      console.log('view:',view_);
+      wrapper.setView(view_);
+      wrapper.draw();
     });
   }
 
@@ -113,7 +191,6 @@
     }
 
     chart.chartClass = klass_name || chart.chartClass
-
     if (typeof chart.dataSource === "string") {
       retrieveRemoteData(chart.element, chart.dataSource, function (data) {
         google.setOnLoadCallback(drawChart(chart, chart.chartClass, chart.element, data, chart.options));
